1ca7533f5c3d20cc9936d2f0c0c5fe3801bcf24a
astralsweat <azmainfiaz@gmail.com>
2020-05-13 06:11:57 +0600

    Hleicon

--- content/js/script.js
+++ content/js/script.js
@@ -67,6 +67,7 @@ function init() {
 playBtn.addEventListener('click', () => {
     mainMenu.style.display = 'none' ;
     overlayMainMenu.classList.remove('active');
+    create10xCards() ;
 });
 
 /*
@@ -80,31 +81,58 @@ document.querySelector( '.js-play-deck-card' ).addEventListener( 'click', () => 
 
 // CARD TYPE-SCORES
 
-const cardTypeScores = [ 
-    pawnpt1_1 = getRandomIntInclusive(1, 3) ,
-    pawnpt1_2 = getRandomIntInclusive(1, 3) ,
-    pawnpt1_3 = getRandomIntInclusive(1, 3) ,
+const cardTypeScores = [
+    pawnpt_1 = 1 ,
+    pawnpt_2 = 2 ,
+    pawnpt_3 = 3 ,
 
-    heavypt1_1 = getRandomIntInclusive(1, 3) ,
-    heavypt1_2 = getRandomIntInclusive(1, 3) ,
-    heavypt1_3 = getRandomIntInclusive(1, 3) ,
+    heavypt_4 = 4 ,
+    heavypt_5 = 5 ,
+    heavypt_6 = 6 ,
 
-    officerpt1_1 = getRandomIntInclusive(1, 3) ,
-    officerpt1_2 = getRandomIntInclusive(1, 3) ,
+    officerpt_7 = 7 ,
+    officerpt_8 = 8 ,
+    officerpt_9 = 9 ,
+    officerpt_10 = 10 ,
 
-    generalpt1_1 = getRandomIntInclusive(1, 3) ,
+    generalpt_9 = 11 ,
+    generalpt_9 = 12 ,
+    generalpt_9 = 13 ,
+    generalpt_9 = 14 ,
 
-    lordpt1_1 = getRandomIntInclusive(1, 3) ,
+    lordpt_10 = 15 ,
+/*
+    specialpt_11 = undefined ,
 
-    specialpt1_1 = getRandomIntInclusive(1, 3) ,
-
-    factionpt1_1 = getRandomIntInclusive(1, 3) ,
-] ;
+    factionpt_12 = undefined,*/
+ ] ;
 
 
 
 // CARD COLLECTION
 
+var cardCreate = {
+    cardPawn_1: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(0,2)] + 'pt-card.png\">'  + '</div>' ,
+    cardPawn_2: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(0,2)] + 'pt-card.png\">'  + '</div>' ,
+    cardPawn_3: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(0,2)] + 'pt-card.png\">'  + '</div>' ,
+
+    
+    cardHeavy_1: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(3,5)] + 'pt-card.png\">'  + '</div>' ,
+    cardHeavy_2: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(3,5)] + 'pt-card.png\">'  + '</div>' ,
+    cardHeavy_3: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(3,5)] + 'pt-card.png\">'  + '</div>' ,
+
+    
+    cardOfficer_1: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(6,9)] + 'pt-card.png\">'  + '</div>' ,
+    cardOfficer_2: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(6,9)] + 'pt-card.png\">'  + '</div>' ,
+    
+    cardGeneral_1: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(10,13)] + 'pt-card.png\">'  + '</div>' ,
+    
+    cardLord_1: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[14] + 'pt-card.png\">'  + '</div>' ,
+    /*
+    cardSpecial_1: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(0,2)] + 'pt-card.png\">'  + '</div>' ,
+    
+    cardFaction_1: '<div class=\"play-card js-play-deck-card\">' + '<img src=\"content/img/cards/' + cardTypeScores[getRandomIntInclusive(0,2)] + 'pt-card.png\">'  + '</div>' ,  */
+}
 
 // __________ RANDOM CARD TYPE & STARTING DECK CREATION
 
@@ -112,11 +140,11 @@ const cardTypeScores = [
 
 
 // __________ CREATE RANDOM STARTING DECK with balanced card types
-/*
+
 function create10xCards() {
-    playDeck.innerHTML = cardCollection[0] + cardCollection[1] + cardCollection[2] + cardCollection[3] + cardCollection[4] + cardCollection[5] + cardCollection[6] + cardCollection[7] + cardCollection[8] + cardCollection[9] ;
+    playDeck.innerHTML = cardCreate.cardPawn_1 + cardCreate.cardPawn_2 + cardCreate.cardPawn_3 + cardCreate.cardHeavy_1 + cardCreate.cardHeavy_2 + cardCreate.cardHeavy_3 + cardCreate.cardOfficer_1 + cardCreate.cardOfficer_2 + cardCreate.cardGeneral_1 + cardCreate.cardLord_1 ;
 }
 
-*/
+
 
 // __________ POP-UP PLAYER'S DECK IN A MODAL for easy navigation
